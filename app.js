00d5a28a07945465c6ab42bd0650349ef91105a5
DanielVG <danielveragi@gmail.com>
2017-08-06 12:07:31 +0200

    Added the event handlers

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+/**
+Entry point for our Application
+*/
+
+/**
+Split my code into Logicall modulues to keep things organized
+Use simple MVC structure
+1. model
+2. views
+3. controlls
+*/
+
 //install the express dependencies
 var express = require("express");
+//require the controller to control the behaviour of the app
+var todoController = require("./controllers/todoController");
 
 //set the express app
 var app = express();
@@ -10,6 +24,9 @@ app.set("view-engine", "ejs");
 //serve static files
 //static files using middleware
 app.use(express.static("./public"));
+
+//fire controllers and give the app as the parameters so the controller can work with it
+todoController(app);
 
 //listen to a specific port
 app.listen(3000);
---
+++ controllers/todoController.js
@@ -0,0 +1,24 @@
+/**
+Here comes everything what controlls the behaviour of the To Do listen
+F.ex mainipulate the data, handling the routes ...
+*/
+
+// export the modile to the app.js file
+modulue.export = function(app){
+  //make the handlers for the different requests we are going to get
+
+  //handler for the request for the url
+  app.get("/todo", function(req, res){
+
+  })
+
+  //handler for the post request of a Button
+  app.post("/todo", function(req, res){
+
+  })
+
+  //handler for a delete request
+  app.delete("/todo", function(req, res){
+    
+  })
+}
