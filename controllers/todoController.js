e7d671ee5bad4eddbffcdc8113d35905bba489bb
DanielVG <danielveragi@gmail.com>
2017-08-08 19:57:12 +0200

    Added the mongoDB database to save the informations there

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -5,9 +5,26 @@ F.ex mainipulate the data, handling the routes ...
 
 //require the body-parser package
 var bodyParser = require("body-parser");
+//require the mongoose package to work with mongoDb
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise; // use native mongoose promises﻿
+
+//connect with the database
+mongoose.connect("mongodb://test:test@ds131061.mlab.com:31061/todo");
+
+//Create Shema - like a Blueprint 
+var todoShema = new mongoose.Schema({
+    item: String
+});
+
+//create a model to base it to the shema
+//now we are able to create new ToDo Items and push them to the data Base
+//What kind of Information we have to expect
+var Todo = mongoose.model("Todo", todoShema);
 
 //make array of Objects to add To-Do List item
-var data = [];
+//var data = [];
+
 //middleware to run in post request 
 // parse application/x-www-form-urlencoded 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -18,28 +35,30 @@ module.exports = function(app){
   //handler for the request for the url
   //render a view when get request to url
   app.get("/todo", function(req, res){
-    //render page
-    //Pass data from the data array to the view
-    res.render("todo", {todos: data});
-
+    //get data from MongoDB and pas it to the view
+    Todo.find({}, function(err, data){
+      //render page
+      //Pass data from the data array to the view
+      res.render("todo", {todos: data});
+    });
   })
 
   //handler for the post request of a Button
   app.post("/todo", urlencodedParser, function(req, res){
-    //push data from the body to the array containing the todo items 
-    data.push(req.body);
-    //send data back to the response and actualize array
-    res.json(data);
+    //get data from the view and add it to mongoDB
+    var newToDo = Todo(req.body).save(function(err, data){
+      if (err) throw err;
+      //send data back to the response and actualize array
+      res.json(data);
+    })
   })
 
   //handler for a delete request
   app.delete("/todo/:item", function(req, res){
-    // update the data in the array
-    data = data.filter(function(todo){
-      // filter out the item we are trying to delete if 
-      return todo.item.replace(/ /g, "-") !== req.params.item;
-    })
-    //send response to the page and actualize the ToDo List 
-    res.json(data);
-  })
+    //delete the requested item from mongoDB
+    Todo.find({item: req.params.item.replace(/\-/g," ")}).remove(function(err, data){
+      if (err) throw err;
+      res.json(data);
+    });
+  });
 }
