ac9c6bcef00037373706c012bd34df1c96f180b8
danielVg <danielveragi@gmail.com>
2018-07-18 16:35:43 +0200

    form errors
    Signed-off-by: danielVg <danielveragi@gmail.com>

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 ## TO-DO Application
 
-This Application is a simple To-Do App based on nodeJS.
-It uses HTML, CSS and Javascript as Frontend technologies.
-In the backend it uses nodeJs and mongoDb as database
+* This Application is a simple To-Do App based on nodeJS.
+* It uses HTML, CSS and Javascript as Frontend technologies.
+* In the backend it uses nodeJs and mongoDb as database
 
 ## Motivation
 
-The motivation of this project is to build my first nodeJs app and get in contact with backend technologies.
+* The motivation of this project is to build my first nodeJs app and get in contact with backend technologies.
 
 ## Installation
 
-Download this project to your machine and run node on it to start the server.
+* Download this project to your machine and run node on it to start the server.
 
 ## Contributors
 
-This project was based on the Youtube Playlist of the Codeninja Channel.
+* This project was based on the Youtube Playlist of the Codeninja Channel.
--- app.js
+++ app.js
@@ -10,27 +10,25 @@ Use simple MVC structure
 3. controlls
 */
 
-//install the express dependencies
-var express = require("express");
-//require the controller to control the behaviour of the app
-var todoController = require("./controllers/todoController");
-//set the express app
-var app = express();
+// install the express dependencies
+let express = require("express");
+// require the controller to control the behaviour of the app
+let todoController = require("./controllers/todoController");
+// set the express app
+let app = express();
 
 
-
-
-//set the ejs template engine
-//the ejs template engine looks inside every view and looks for the respective file
+// set the ejs template engine
+// the ejs template engine looks inside every view and looks for the respective file
 app.set("view engine", "ejs");
 
-//serve static files
-//static files using middleware
+// serve static files
+// static files using middleware
 app.use(express.static("./public"));
 
-//fire controllers and give the app as the parameters so the controller can work with it
+// fire controllers and give the app as the parameters so the controller can work with it
 todoController(app);
 
-//listen to a specific port
+// listen to a specific port
 app.listen(3000);
 console.log("Now you are listening to port 3000");
